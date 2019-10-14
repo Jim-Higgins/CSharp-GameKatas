4d4a9717b714e2a277b46d55949d979849aec0d4
Jim Higgins <Jim.Higgins@onthebeach.co.uk>
2019-10-14 13:00:19 +0100

    working changes

--- GameZone/src/GameZone/SuspensionMan.cs
+++ GameZone/src/GameZone/SuspensionMan.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 
 namespace GameZone
 {
@@ -6,8 +7,23 @@ namespace GameZone
     {
         static void Main(string[] args)
         {
-            // Info about the game 
+            // Info about the game
+            Console.WriteLine("Welcome to...");
+            Thread.Sleep(1000); // adding in Suspense for the player
+            Console.WriteLine("...Suspension Man");
+
             // a list of hard coded words to begin with
+            string[] words = new string[]{
+                "Fox", "Rabbit", "Fish"
+            };
+
+            // Game will randomly pick one of the words
+            Random rnd = new Random();
+            int wordSeed = rnd.Next(words.Length);
+            string selectedWord = words[wordSeed];
+            
+            Console.WriteLine("");
+            Console.WriteLine($"The selected word is...{selectedWord}");
             // the game will tell you the length of the word
             // player enters a letter 
             // game says if the letter is present: 

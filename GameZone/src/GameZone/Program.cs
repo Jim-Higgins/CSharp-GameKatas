528d9921c7f55b0137ef37a915fc02275b904e12
Jim Higgins <Jim.Higgins@onthebeach.co.uk>
2019-10-15 12:02:20 +0100

    updated menu selector with NC

--- GameZone/src/GameZone/Program.cs
+++ GameZone/src/GameZone/Program.cs
@@ -1,36 +1,75 @@
 using System;
+using System.Collections.Generic;
 
-namespace GameZone {
-    class Program {
-        static void Main (string[] args) {
-            Console.Clear ();
-            Console.WriteLine ("Select your game choices: \n1)JimsV \n2)NorthCodersV ");
-            string selectedGameType = Console.ReadLine ();
-            if (selectedGameType == "1") {
-                Console.Clear ();
-                Console.WriteLine ("Jims Version of the games");
-                var mySpinner = new Loader ();
-                mySpinner.SimplePercentageLoader ();
+namespace GameZone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mySpinner = new Loader();
+            Console.Clear();
+            Console.WriteLine("Select your game choices: \n1)JimsV \n2)NorthCodersV ");
+            string selectedGameType = Console.ReadLine();
 
-                Console.Clear ();
-                Console.WriteLine ("Select your game:");
-                Console.WriteLine ("1) SimonSays \n2) SuspensionMan"); // add in list of games here
-                string selectedGame = Console.ReadLine ();
-                if (selectedGame == "1") {
-                    Console.WriteLine ("Simon picked");
-                    var myGame = new SimonSays ();
-                    myGame.Play (args);
+            // <----- For all the my written games -------
+
+            if (selectedGameType == "1")
+            {
+                Console.Clear();
+                Console.WriteLine("Jims Version of the games");
+                mySpinner.SimplePercentageLoader();
+
+                Console.Clear();
+                Console.WriteLine("Select your game:");
+                Console.WriteLine("1) SimonSays \n2) SuspensionMan"); // add in list of games here
+                string selectedGame = Console.ReadLine();
+
+                if (selectedGame == "1")
+                {
+                    Console.WriteLine("Simon picked");
+                    var myGame = new SimonSays();
+                    myGame.Play(args);
                 }
-                if (selectedGame == "2") {
-                    var myGame = new SuspensionManJims ();
-                    myGame.Play ();
-                }
 
+                if (selectedGame == "2")
+                {
+                    var myGame = new SuspensionManJims();
+                    myGame.Play();
+                }
             }
-            if (selectedGameType == "2") {
-                Console.Clear ();
-                Console.WriteLine ("NorthCoders Version of the games");
 
+            // <----- For all the northCoder written games -------
+
+            if (selectedGameType == "2")
+            {
+                Console.Clear();
+                Console.WriteLine("NorthCoders Version of the games");
+                mySpinner.SimplePercentageLoader();
+
+                Console.Clear();
+                Console.WriteLine("Select your game:");
+                Console.WriteLine("1) na \n2) SuspensionMan"); // add in list of games here
+                string selectedGame = Console.ReadLine();
+
+                if (selectedGame == "1")
+                {
+                    Console.WriteLine("Simon picked");
+                    var myGame = new SimonSays();
+                    myGame.Play(args);
+                }
+
+                if (selectedGame == "2")
+                {
+                    string[] words = Console.ReadLine().Split(' ');
+                    List<string> wordList = new List<string>();
+                    foreach (string word in words)
+                    {
+                        wordList.Add(word);
+                    }
+                    var myGame = new SuspendPersonNC();
+                    myGame.Play(wordList);
+                }
             }
         }
     }
---
+++ GameZone/tests/GameZone.Tests/GameZoneUnitTests.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GameZone.Tests {
+  public class GameZoneTests {
+    [Fact]
+    public void GetValidRandomSeedReturnsValidInt () {
+      // Arrange
+      var testGame = new SuspendPerson ();
+      int max = 4;
+
+      // Act
+      int seed = testGame.GetValidRandomSeed (max);
+
+      // Assert
+      Assert.InRange (seed, 0, 3);
+    }
+
+    [Fact]
+    public void ReturnsCharWhenFoundInList () {
+      // Arrange
+      var testGame = new SuspendPerson ();
+      var guesses = new List<char> () { 'a', 'b', 'c' };
+      char guess = 'a';
+
+      // Act
+      char actual = testGame.GetCharacterToPrint (guesses, guess);
+
+      // Assert
+      Assert.Equal ('a', actual);
+    }
+
+    [Fact]
+    public void ReturnsDashWhenNotFoundInList () {
+      // Arrange
+      var testGame = new SuspendPerson ();
+      var guesses = new List<char> () { 'a', 'b', 'c' };
+      char guess = 'd';
+
+      // Act
+      char actual = testGame.GetCharacterToPrint (guesses, guess);
+
+      // Assert
+      Assert.Equal ('-', actual);
+    }
+  }
+}
